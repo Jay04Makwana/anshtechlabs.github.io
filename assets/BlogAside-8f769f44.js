e21747dfbdc5c810b6e538275be7208b81ac6891
Jay Makwana <jaimakwana786@gmail.com>
2024-01-10 14:37:49 +0530

    ui changes

--- assets/BlogAside-8f769f44.js
+++ assets/BlogAside-8f769f44.js
@@ -1,1 +1,294 @@
-import{_ as i,o as r,c as o,b as t,m as w,t as a,s as g,n as S,r as x,j as m,u as p,F as _,k as v,i as f,h as u}from"./index-32023084.js";const D={props:["archives","lastItem"]},$=t("i",{class:"pi pi-star-fill text-xred pr-3"},null,-1),z={href:"#",class:"hover:text-xred"},B={class:"float-right"};function C(d,c,e,n,b,h){return r(),o("li",{class:g(["border-b border-solid border-zinc-100 pb-2.5 mb-2.5",{"border-none":e.lastItem,"pb-0":e.lastItem,"mb-0":e.lastItem}])},[$,t("a",z,[w(a(e.archives.month),1),t("span",B,a(e.archives.year),1)])],2)}const P=i(D,[["render",C]]),R={props:["postData","lastItem"]},N={href:"#",class:"float-left h-[130px] w-[130px] mr-[15px] relative overflow-hidden"},A={class:"overflow-hidden"},T={datetime:"2022-06-30",class:"block text-[13px] text-[#929292]"},V={class:"font-barlow font-semibold text-md text-[#474c40] inline-block"},E={href:"#",class:"hover:text-xred dark:text-white"},F=t("div",{class:"clear"},null,-1);function L(d,c,e,n,b,h){return r(),o("article",{class:g(["overflow-hidden mb-2.5 pb-2.5 border-b border-solid border-[#efefef]",{"border-none":e.lastItem,"pb-0":e.lastItem,"mb-0":e.lastItem}])},[t("a",N,[t("span",{class:"h-[130px] w-[130px] inline-block bg-cover bg-repeat bg-center relative bg-[#0e0129]",role:"img",style:S({"background-image":`url('/images/popular-posts/${e.postData.postImage}')`})},null,4)]),t("div",A,[t("time",T,a(e.postData.postDate),1),t("h4",V,[t("a",E,a(e.postData.postText),1)])]),F],2)}const W=i(R,[["render",L]]),j={props:["category","lastItem"]},G=t("i",{class:"pi pi-star-fill text-xred pr-3"},null,-1),M={href:"#",class:"hover:text-xred"},q={class:"float-right"};function H(d,c,e,n,b,h){return r(),o("li",{class:g(["border-b border-solid border-zinc-100 pb-2.5 mb-2.5",{"border-none":e.lastItem,"pb-0":e.lastItem,"mb-0":e.lastItem}])},[G,t("a",M,[w(a(e.category.name)+" ",1),t("span",q,"("+a(e.category.amount)+")",1)])],2)}const J=i(j,[["render",H]]),K={components:{PopularPosts:W,BlogArchives:P,blogCategories:J},props:["postData","archives","category","meta"]},O={class:"dark:text-white"},Q=u('<div><div class="text-xl font-semibold font-barlow border-b border-solid border-gray-200 pb-2.5 text-xpurple dark:text-white"> Search Now</div><div class="pt-5"><form class="relative"><label><input type="search" class="bg-transparent h-12 w-full py-1.5 px-3.5 outline-none border border-solid border-gray-200" placeholder="Search..."></label><button type="submit" class="absolute right-0 bottom-0 h-12 w-12 border-none text-white bg-xred"><i class="pi pi-search"></i></button></form></div></div>',1),U={class:"mt-[35px] relative overflow-hidden"},X=t("h3",{class:"text-xl font-semibold font-barlow mb-0 border-b border-solid border-gray-200 pb-2.5 relative text-xpurple dark:text-white"}," Popular Posts",-1),Y={class:"pt-5 last:mb-0 last:pb-0"},Z={class:"mt-[35px]"},ee=t("h3",{class:"text-xl border-b border-solid border-gray-200 pb-2.5 text-xpurple font-semibold font-barlow relative dark:text-white"}," Archives",-1),te={class:"pt-5"},re={class:"text-sm text-stone-500 dark:text-white"},se={class:"mt-[35px]"},oe=t("h3",{class:"text-xl border-b border-solid border-gray-200 pb-2.5 text-xpurple font-semibold font-barlow relative dark:text-white"}," Categories",-1),ae={class:"pt-5"},le={class:"text-sm text-stone-500 dark:text-white"},ie={class:"mt-[35px]"},de=u('<h3 class="text-xl border-b border-solid border-gray-200 pb-2.5 text-xpurple font-semibold font-barlow relative dark:text-white"> Meta</h3><div class="pt-5"><ul class="text-stone-500 dark:text-white text-md"><li class="border-b border-solid border-zinc-100 pb-2.5 mb-2.5"><i class="pi pi-star-fill text-xred pr-3"></i><a href="#" class="hover:text-xred">Log in</a></li><li class="border-b border-solid border-zinc-100 pb-2.5 mb-2.5"><i class="pi pi-star-fill text-xred pr-3"></i><a href="#" class="hover:text-xred">Entries <abbr title="Really Simple Syndication">RSS</abbr></a></li><li class="border-b border-solid border-zinc-100 pb-2.5 mb-2.5"><i class="pi pi-star-fill text-xred pr-3"></i><a href="#" class="hover:text-xred">Comments <abbr title="Really Simple Syndication">RSS</abbr></a></li><li class="border-b border-solid border-zinc-100 pb-2.5 mb-2.5"><i class="pi pi-star-fill text-xred pr-3"></i><a href="#" class="hover:text-xred">WordPress.org</a></li></ul></div>',2),ce=[de],ne=u('<section class="mt-[35px]"><h3 class="text-xl border-b border-solid border-gray-200 pb-2.5 text-xpurple font-semibold font-barlow relative dark:text-white"> Tags</h3><div class="pt-5"><div><a class="inline-block text-sm text-stone-500 py-2 px-2.5 border border-dashed border-gray-200 mt-2.5 mr-2.5 hover:text-white hover:bg-xred dark:text-white" href="#">Blockchain (3)</a><a class="inline-block text-sm text-stone-500 py-2 px-2.5 border border-dashed border-gray-200 mt-2.5 mr-2.5 hover:text-white hover:bg-xred dark:text-white" href="#">Cyber security (3)</a><a class="inline-block text-sm text-stone-500 py-2 px-2.5 border border-dashed border-gray-200 mt-2.5 mr-2.5 hover:text-white hover:bg-xred dark:text-white" href="#">Cybercrime (2)</a><a class="inline-block text-sm text-stone-500 py-2 px-2.5 border border-dashed border-gray-200 mt-2.5 mr-2.5 hover:text-white hover:bg-xred dark:text-white" href="#">Global news (2)</a><a class="inline-block text-sm text-stone-500 py-2 px-2.5 border border-dashed border-gray-200 mt-2.5 mr-2.5 hover:text-white hover:bg-xred dark:text-white" href="#">Ransomware (1)</a><a class="inline-block text-sm text-stone-500 py-2 px-2.5 border border-dashed border-gray-200 mt-2.5 mr-2.5 hover:text-white hover:bg-xred dark:text-white" href="#">Whitepapers (2) </a></div></div></section>',1);function be(d,c,e,n,b,h){const y=x("popular-posts"),k=x("blog-archives"),I=x("blog-categories");return r(),o("aside",O,[Q,m(t("section",U,[X,t("div",Y,[(r(!0),o(_,null,v(e.postData,(l,s)=>(r(),f(y,{key:s,postData:l,lastItem:s===e.postData.length-1},null,8,["postData","lastItem"]))),128))])],512),[[p,e.postData]]),m(t("section",Z,[ee,t("div",te,[t("ul",re,[(r(!0),o(_,null,v(e.archives,(l,s)=>(r(),f(k,{key:s,archives:l,lastItem:s===e.archives.length-1},null,8,["archives","lastItem"]))),128))])])],512),[[p,e.archives]]),t("section",se,[oe,t("div",ae,[t("ul",le,[(r(!0),o(_,null,v(e.category,(l,s)=>(r(),f(I,{key:s,category:l,lastItem:s===e.category.length-1},null,8,["category","lastItem"]))),128))])])]),m(t("section",ie,ce,512),[[p,e.meta]]),ne])}const xe=i(K,[["render",be]]);export{xe as B};
+import {
+  _ as i,
+  o as r,
+  c as o,
+  b as t,
+  m as w,
+  t as a,
+  s as g,
+  n as S,
+  r as x,
+  j as m,
+  u as p,
+  F as _,
+  k as v,
+  i as f,
+  h as u,
+} from './index-32023084.js'
+const D = { props: ['archives', 'lastItem'] },
+  $ = t('i', { class: 'pi pi-star-fill text-xred pr-3' }, null, -1),
+  z = { href: '#', class: 'hover:text-xred' },
+  B = { class: 'float-right' }
+function C(d, c, e, n, b, h) {
+  return (
+    r(),
+    o(
+      'li',
+      {
+        class: g([
+          'border-b border-solid border-zinc-100 pb-2.5 mb-2.5',
+          { 'border-none': e.lastItem, 'pb-0': e.lastItem, 'mb-0': e.lastItem },
+        ]),
+      },
+      [
+        $,
+        t('a', z, [
+          w(a(e.archives.month), 1),
+          t('span', B, a(e.archives.year), 1),
+        ]),
+      ],
+      2
+    )
+  )
+}
+const P = i(D, [['render', C]]),
+  R = { props: ['postData', 'lastItem'] },
+  N = {
+    href: '#',
+    class: 'float-left h-[130px] w-[130px] mr-[15px] relative overflow-hidden',
+  },
+  A = { class: 'overflow-hidden' },
+  T = { datetime: '2022-06-30', class: 'block text-[13px] text-[#929292]' },
+  V = {
+    class: 'font-barlow font-semibold text-md text-[#474c40] inline-block',
+  },
+  E = { href: '#', class: 'hover:text-xred dark:text-white' },
+  F = t('div', { class: 'clear' }, null, -1)
+function L(d, c, e, n, b, h) {
+  return (
+    r(),
+    o(
+      'article',
+      {
+        class: g([
+          'overflow-hidden mb-2.5 pb-2.5 border-b border-solid border-[#efefef]',
+          { 'border-none': e.lastItem, 'pb-0': e.lastItem, 'mb-0': e.lastItem },
+        ]),
+      },
+      [
+        t('a', N, [
+          t(
+            'span',
+            {
+              class:
+                'h-[130px] w-[130px] inline-block bg-cover bg-repeat bg-center relative bg-[#0e0129]',
+              role: 'img',
+              style: S({
+                'background-image': `url('images/popular-posts/${e.postData.postImage}')`,
+              }),
+            },
+            null,
+            4
+          ),
+        ]),
+        t('div', A, [
+          t('time', T, a(e.postData.postDate), 1),
+          t('h4', V, [t('a', E, a(e.postData.postText), 1)]),
+        ]),
+        F,
+      ],
+      2
+    )
+  )
+}
+const W = i(R, [['render', L]]),
+  j = { props: ['category', 'lastItem'] },
+  G = t('i', { class: 'pi pi-star-fill text-xred pr-3' }, null, -1),
+  M = { href: '#', class: 'hover:text-xred' },
+  q = { class: 'float-right' }
+function H(d, c, e, n, b, h) {
+  return (
+    r(),
+    o(
+      'li',
+      {
+        class: g([
+          'border-b border-solid border-zinc-100 pb-2.5 mb-2.5',
+          { 'border-none': e.lastItem, 'pb-0': e.lastItem, 'mb-0': e.lastItem },
+        ]),
+      },
+      [
+        G,
+        t('a', M, [
+          w(a(e.category.name) + ' ', 1),
+          t('span', q, '(' + a(e.category.amount) + ')', 1),
+        ]),
+      ],
+      2
+    )
+  )
+}
+const J = i(j, [['render', H]]),
+  K = {
+    components: { PopularPosts: W, BlogArchives: P, blogCategories: J },
+    props: ['postData', 'archives', 'category', 'meta'],
+  },
+  O = { class: 'dark:text-white' },
+  Q = u(
+    '<div><div class="text-xl font-semibold font-barlow border-b border-solid border-gray-200 pb-2.5 text-xpurple dark:text-white"> Search Now</div><div class="pt-5"><form class="relative"><label><input type="search" class="bg-transparent h-12 w-full py-1.5 px-3.5 outline-none border border-solid border-gray-200" placeholder="Search..."></label><button type="submit" class="absolute right-0 bottom-0 h-12 w-12 border-none text-white bg-xred"><i class="pi pi-search"></i></button></form></div></div>',
+    1
+  ),
+  U = { class: 'mt-[35px] relative overflow-hidden' },
+  X = t(
+    'h3',
+    {
+      class:
+        'text-xl font-semibold font-barlow mb-0 border-b border-solid border-gray-200 pb-2.5 relative text-xpurple dark:text-white',
+    },
+    ' Popular Posts',
+    -1
+  ),
+  Y = { class: 'pt-5 last:mb-0 last:pb-0' },
+  Z = { class: 'mt-[35px]' },
+  ee = t(
+    'h3',
+    {
+      class:
+        'text-xl border-b border-solid border-gray-200 pb-2.5 text-xpurple font-semibold font-barlow relative dark:text-white',
+    },
+    ' Archives',
+    -1
+  ),
+  te = { class: 'pt-5' },
+  re = { class: 'text-sm text-stone-500 dark:text-white' },
+  se = { class: 'mt-[35px]' },
+  oe = t(
+    'h3',
+    {
+      class:
+        'text-xl border-b border-solid border-gray-200 pb-2.5 text-xpurple font-semibold font-barlow relative dark:text-white',
+    },
+    ' Categories',
+    -1
+  ),
+  ae = { class: 'pt-5' },
+  le = { class: 'text-sm text-stone-500 dark:text-white' },
+  ie = { class: 'mt-[35px]' },
+  de = u(
+    '<h3 class="text-xl border-b border-solid border-gray-200 pb-2.5 text-xpurple font-semibold font-barlow relative dark:text-white"> Meta</h3><div class="pt-5"><ul class="text-stone-500 dark:text-white text-md"><li class="border-b border-solid border-zinc-100 pb-2.5 mb-2.5"><i class="pi pi-star-fill text-xred pr-3"></i><a href="#" class="hover:text-xred">Log in</a></li><li class="border-b border-solid border-zinc-100 pb-2.5 mb-2.5"><i class="pi pi-star-fill text-xred pr-3"></i><a href="#" class="hover:text-xred">Entries <abbr title="Really Simple Syndication">RSS</abbr></a></li><li class="border-b border-solid border-zinc-100 pb-2.5 mb-2.5"><i class="pi pi-star-fill text-xred pr-3"></i><a href="#" class="hover:text-xred">Comments <abbr title="Really Simple Syndication">RSS</abbr></a></li><li class="border-b border-solid border-zinc-100 pb-2.5 mb-2.5"><i class="pi pi-star-fill text-xred pr-3"></i><a href="#" class="hover:text-xred">WordPress.org</a></li></ul></div>',
+    2
+  ),
+  ce = [de],
+  ne = u(
+    '<section class="mt-[35px]"><h3 class="text-xl border-b border-solid border-gray-200 pb-2.5 text-xpurple font-semibold font-barlow relative dark:text-white"> Tags</h3><div class="pt-5"><div><a class="inline-block text-sm text-stone-500 py-2 px-2.5 border border-dashed border-gray-200 mt-2.5 mr-2.5 hover:text-white hover:bg-xred dark:text-white" href="#">Blockchain (3)</a><a class="inline-block text-sm text-stone-500 py-2 px-2.5 border border-dashed border-gray-200 mt-2.5 mr-2.5 hover:text-white hover:bg-xred dark:text-white" href="#">Cyber security (3)</a><a class="inline-block text-sm text-stone-500 py-2 px-2.5 border border-dashed border-gray-200 mt-2.5 mr-2.5 hover:text-white hover:bg-xred dark:text-white" href="#">Cybercrime (2)</a><a class="inline-block text-sm text-stone-500 py-2 px-2.5 border border-dashed border-gray-200 mt-2.5 mr-2.5 hover:text-white hover:bg-xred dark:text-white" href="#">Global news (2)</a><a class="inline-block text-sm text-stone-500 py-2 px-2.5 border border-dashed border-gray-200 mt-2.5 mr-2.5 hover:text-white hover:bg-xred dark:text-white" href="#">Ransomware (1)</a><a class="inline-block text-sm text-stone-500 py-2 px-2.5 border border-dashed border-gray-200 mt-2.5 mr-2.5 hover:text-white hover:bg-xred dark:text-white" href="#">Whitepapers (2) </a></div></div></section>',
+    1
+  )
+function be(d, c, e, n, b, h) {
+  const y = x('popular-posts'),
+    k = x('blog-archives'),
+    I = x('blog-categories')
+  return (
+    r(),
+    o('aside', O, [
+      Q,
+      m(
+        t(
+          'section',
+          U,
+          [
+            X,
+            t('div', Y, [
+              (r(!0),
+              o(
+                _,
+                null,
+                v(
+                  e.postData,
+                  (l, s) => (
+                    r(),
+                    f(
+                      y,
+                      {
+                        key: s,
+                        postData: l,
+                        lastItem: s === e.postData.length - 1,
+                      },
+                      null,
+                      8,
+                      ['postData', 'lastItem']
+                    )
+                  )
+                ),
+                128
+              )),
+            ]),
+          ],
+          512
+        ),
+        [[p, e.postData]]
+      ),
+      m(
+        t(
+          'section',
+          Z,
+          [
+            ee,
+            t('div', te, [
+              t('ul', re, [
+                (r(!0),
+                o(
+                  _,
+                  null,
+                  v(
+                    e.archives,
+                    (l, s) => (
+                      r(),
+                      f(
+                        k,
+                        {
+                          key: s,
+                          archives: l,
+                          lastItem: s === e.archives.length - 1,
+                        },
+                        null,
+                        8,
+                        ['archives', 'lastItem']
+                      )
+                    )
+                  ),
+                  128
+                )),
+              ]),
+            ]),
+          ],
+          512
+        ),
+        [[p, e.archives]]
+      ),
+      t('section', se, [
+        oe,
+        t('div', ae, [
+          t('ul', le, [
+            (r(!0),
+            o(
+              _,
+              null,
+              v(
+                e.category,
+                (l, s) => (
+                  r(),
+                  f(
+                    I,
+                    {
+                      key: s,
+                      category: l,
+                      lastItem: s === e.category.length - 1,
+                    },
+                    null,
+                    8,
+                    ['category', 'lastItem']
+                  )
+                )
+              ),
+              128
+            )),
+          ]),
+        ]),
+      ]),
+      m(t('section', ie, ce, 512), [[p, e.meta]]),
+      ne,
+    ])
+  )
+}
+const xe = i(K, [['render', be]])
+export { xe as B }
--- assets/Index-214ea5cc.js
+++ assets/Index-214ea5cc.js
@@ -1,1 +1,102 @@
-import{B as d}from"./BlogAside-8f769f44.js";import{_ as s,r as o,o as l,c as m,b as a,d as u,h as x,i as g}from"./index-32023084.js";const b="/images/blog/7.jpg",h="/images/blog/8.jpg",f="/images/blog/9.jpg",v={components:{BlogAside:d},data(){return{postData:[{postImage:"popular-posts-1.png",postDate:"April 20, 2022",postText:"Drughydrus Add Google Drive To Roughrobin Torjan"},{postImage:"popular-posts-2.png",postDate:"Jun 21, 2022",postText:"DHS Issues Emergency Directive To Prevent Hacking Attack"},{postImage:"popular-posts-3.png",postDate:"Jun 22, 2022",postText:"Security In A Fragment World Of Workload"}],archiveData:[{month:"February",year:"2022"},{month:"March",year:"2022"},{month:"April",year:"2022"},{month:"May",year:"2022"},{month:"June",year:"2022"}],categoryData:[{name:"Blockchain",amount:"10"},{name:"Cyber Security",amount:"20"},{name:"Cybercrime",amount:"10"},{name:"Global news",amount:"12"},{name:"RansomeWare",amount:"16"}]}}},_={class:"py-24"},y={class:"container mx-auto"},w={class:"grid grid-cols-12"},k=x('<div class="col-span-12 lg:col-span-8"><div><div class="grid grid-cols-12"><div class="col-span-12 md:col-span-6 lg:col-span-12 px-3"><div class="relative mb-8 transform hover:-translate-y-1.5 duration-500"><div class="absolute top-0 left-0 w-full h-full bg-gradient-to-b from-transparent to-[#0e0129fc]"></div><img src="'+b+'" class="max-w-full h-auto" alt="Image"><div class="absolute left-0 right-0 bottom-0 p-[30px] text-center lg:text-left"><h3 class="font-barlow mb-3 font-semibold text-xl lg:text-2xl"><a href="#" class="text-white">DHS Issues Emergency Directive To Prevent Hacking Attack</a></h3><p class="text-white mb-3.5 text-sm lg:text-base">Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, cum excepturi ipsum, asperiores placeat quidem deleniti doloremque neque, voluptas ut earum atque quo voluptatibus. Autem.</p><a href="#" class="block text-xs font-semibold text-xred dark:text-white"> Read More </a></div></div></div><div class="col-span-12 md:col-span-6 lg:col-span-12 px-3"><div class="relative mb-8 transform hover:-translate-y-1.5 duration-500"><div class="absolute top-0 left-0 w-full h-full bg-gradient-to-b from-transparent to-[#0e0129fc]"></div><img src="'+h+'" class="max-w-full h-auto" alt="Image"><div class="absolute left-0 right-0 bottom-0 p-[30px] text-center lg:text-left"><h3 class="font-barlow mb-3 font-semibold text-xl lg:text-2xl"><a href="#" class="text-white">Drughydrus Add Google Drive To Roughrobin Torjan</a></h3><p class="text-white mb-3.5 text-sm lg:text-base">Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, cum excepturi ipsum, asperiores placeat quidem deleniti doloremque neque, voluptas ut earum atque quo voluptatibus. Autem.</p><a href="#" class="block text-xs font-semibold text-xred dark:text-white"> Read More </a></div></div></div><div class="col-span-12 md:col-span-6 md:col-start-4 lg:col-span-12 px-3"><div class="relative mb-8 transform hover:-translate-y-1.5 duration-500"><div class="absolute top-0 left-0 w-full h-full bg-gradient-to-b from-transparent to-[#0e0129fc]"></div><img src="'+f+'" class="max-w-full h-auto" alt="Image"><div class="absolute left-0 right-0 bottom-0 p-[30px] text-center lg:text-left"><h3 class="font-barlow mb-3 font-semibold text-xl lg:text-2xl"><a href="#" class="text-white">Security In A Fragment World Of Workload</a></h3><p class="text-white mb-3.5 text-sm lg:text-base">Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, cum excepturi ipsum, asperiores placeat quidem deleniti doloremque neque, voluptas ut earum atque quo voluptatibus. Autem.</p><a href="#" class="block text-xs font-semibold text-xred dark:text-white"> Read More </a></div></div></div><div class="col-span-12 px-3"><div class="flex justify-center mt-5"><ul class="inline-block text-xred font-bold"><li class="page-item inline-block px-2"><a class="px-4 py-2 shadow-[0_0_15px_rgba(208,208,208,1)] bg-white rounded-l" href="#"> « </a></li><li class="page-item inline-block px-2"><a class="px-4 py-2 shadow-[0_0_15px_rgba(208,208,208,1)] bg-xred text-white" href="#">1</a></li><li class="page-item inline-block px-2"><a class="px-4 py-2 shadow-[0_0_15px_rgba(208,208,208,1)] bg-white" href="#">2</a></li><li class="page-item inline-block px-2"><a class="px-4 py-2 shadow-[0_0_15px_rgba(208,208,208,1)] bg-white" href="#">3</a></li><li class="page-item inline-block px-2"><a class="px-4 py-2 shadow-[0_0_15px_rgba(208,208,208,1)] bg-white rounded-r" href="#"> » </a></li></ul></div></div></div></div></div>',1),D={class:"col-span-12 lg:col-span-4 px-3 max-lg:mt-5"};function q(i,r,c,n,e,p){const t=o("blog-aside");return l(),m("section",_,[a("div",y,[a("div",w,[k,a("div",D,[u(t,{postData:e.postData,archives:e.archiveData,category:e.categoryData,meta:"true"},null,8,["postData","archives","category"])])])])])}const A=s(v,[["render",q]]),I={components:{BlogIndex:A}};function B(i,r,c,n,e,p){const t=o("blog-index");return l(),g(t)}const j=s(I,[["render",B]]);export{j as default};
+import { B as d } from './BlogAside-8f769f44.js'
+import {
+  _ as s,
+  r as o,
+  o as l,
+  c as m,
+  b as a,
+  d as u,
+  h as x,
+  i as g,
+} from './index-32023084.js'
+const b = 'images/blog/7.jpg',
+  h = 'images/blog/8.jpg',
+  f = 'images/blog/9.jpg',
+  v = {
+    components: { BlogAside: d },
+    data() {
+      return {
+        postData: [
+          {
+            postImage: 'popular-posts-1.png',
+            postDate: 'April 20, 2022',
+            postText: 'Drughydrus Add Google Drive To Roughrobin Torjan',
+          },
+          {
+            postImage: 'popular-posts-2.png',
+            postDate: 'Jun 21, 2022',
+            postText:
+              'DHS Issues Emergency Directive To Prevent Hacking Attack',
+          },
+          {
+            postImage: 'popular-posts-3.png',
+            postDate: 'Jun 22, 2022',
+            postText: 'Security In A Fragment World Of Workload',
+          },
+        ],
+        archiveData: [
+          { month: 'February', year: '2022' },
+          { month: 'March', year: '2022' },
+          { month: 'April', year: '2022' },
+          { month: 'May', year: '2022' },
+          { month: 'June', year: '2022' },
+        ],
+        categoryData: [
+          { name: 'Blockchain', amount: '10' },
+          { name: 'Cyber Security', amount: '20' },
+          { name: 'Cybercrime', amount: '10' },
+          { name: 'Global news', amount: '12' },
+          { name: 'RansomeWare', amount: '16' },
+        ],
+      }
+    },
+  },
+  _ = { class: 'py-24' },
+  y = { class: 'container mx-auto' },
+  w = { class: 'grid grid-cols-12' },
+  k = x(
+    '<div class="col-span-12 lg:col-span-8"><div><div class="grid grid-cols-12"><div class="col-span-12 md:col-span-6 lg:col-span-12 px-3"><div class="relative mb-8 transform hover:-translate-y-1.5 duration-500"><div class="absolute top-0 left-0 w-full h-full bg-gradient-to-b from-transparent to-[#0e0129fc]"></div><img src="' +
+      b +
+      '" class="max-w-full h-auto" alt="Image"><div class="absolute left-0 right-0 bottom-0 p-[30px] text-center lg:text-left"><h3 class="font-barlow mb-3 font-semibold text-xl lg:text-2xl"><a href="#" class="text-white">DHS Issues Emergency Directive To Prevent Hacking Attack</a></h3><p class="text-white mb-3.5 text-sm lg:text-base">Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, cum excepturi ipsum, asperiores placeat quidem deleniti doloremque neque, voluptas ut earum atque quo voluptatibus. Autem.</p><a href="#" class="block text-xs font-semibold text-xred dark:text-white"> Read More </a></div></div></div><div class="col-span-12 md:col-span-6 lg:col-span-12 px-3"><div class="relative mb-8 transform hover:-translate-y-1.5 duration-500"><div class="absolute top-0 left-0 w-full h-full bg-gradient-to-b from-transparent to-[#0e0129fc]"></div><img src="' +
+      h +
+      '" class="max-w-full h-auto" alt="Image"><div class="absolute left-0 right-0 bottom-0 p-[30px] text-center lg:text-left"><h3 class="font-barlow mb-3 font-semibold text-xl lg:text-2xl"><a href="#" class="text-white">Drughydrus Add Google Drive To Roughrobin Torjan</a></h3><p class="text-white mb-3.5 text-sm lg:text-base">Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, cum excepturi ipsum, asperiores placeat quidem deleniti doloremque neque, voluptas ut earum atque quo voluptatibus. Autem.</p><a href="#" class="block text-xs font-semibold text-xred dark:text-white"> Read More </a></div></div></div><div class="col-span-12 md:col-span-6 md:col-start-4 lg:col-span-12 px-3"><div class="relative mb-8 transform hover:-translate-y-1.5 duration-500"><div class="absolute top-0 left-0 w-full h-full bg-gradient-to-b from-transparent to-[#0e0129fc]"></div><img src="' +
+      f +
+      '" class="max-w-full h-auto" alt="Image"><div class="absolute left-0 right-0 bottom-0 p-[30px] text-center lg:text-left"><h3 class="font-barlow mb-3 font-semibold text-xl lg:text-2xl"><a href="#" class="text-white">Security In A Fragment World Of Workload</a></h3><p class="text-white mb-3.5 text-sm lg:text-base">Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, cum excepturi ipsum, asperiores placeat quidem deleniti doloremque neque, voluptas ut earum atque quo voluptatibus. Autem.</p><a href="#" class="block text-xs font-semibold text-xred dark:text-white"> Read More </a></div></div></div><div class="col-span-12 px-3"><div class="flex justify-center mt-5"><ul class="inline-block text-xred font-bold"><li class="page-item inline-block px-2"><a class="px-4 py-2 shadow-[0_0_15px_rgba(208,208,208,1)] bg-white rounded-l" href="#"> « </a></li><li class="page-item inline-block px-2"><a class="px-4 py-2 shadow-[0_0_15px_rgba(208,208,208,1)] bg-xred text-white" href="#">1</a></li><li class="page-item inline-block px-2"><a class="px-4 py-2 shadow-[0_0_15px_rgba(208,208,208,1)] bg-white" href="#">2</a></li><li class="page-item inline-block px-2"><a class="px-4 py-2 shadow-[0_0_15px_rgba(208,208,208,1)] bg-white" href="#">3</a></li><li class="page-item inline-block px-2"><a class="px-4 py-2 shadow-[0_0_15px_rgba(208,208,208,1)] bg-white rounded-r" href="#"> » </a></li></ul></div></div></div></div></div>',
+    1
+  ),
+  D = { class: 'col-span-12 lg:col-span-4 px-3 max-lg:mt-5' }
+function q(i, r, c, n, e, p) {
+  const t = o('blog-aside')
+  return (
+    l(),
+    m('section', _, [
+      a('div', y, [
+        a('div', w, [
+          k,
+          a('div', D, [
+            u(
+              t,
+              {
+                postData: e.postData,
+                archives: e.archiveData,
+                category: e.categoryData,
+                meta: 'true',
+              },
+              null,
+              8,
+              ['postData', 'archives', 'category']
+            ),
+          ]),
+        ]),
+      ]),
+    ])
+  )
+}
+const A = s(v, [['render', q]]),
+  I = { components: { BlogIndex: A } }
+function B(i, r, c, n, e, p) {
+  const t = o('blog-index')
+  return l(), g(t)
+}
+const j = s(I, [['render', B]])
+export { j as default }
